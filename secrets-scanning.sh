17f2da10dc82afac9d0132e3a013b4eed380d881
Alex Gendry <alex.gendry@microfocus.com>
2022-12-15 12:50:06 +0000

    pre-commit

--- secrets-scanning.sh
+++ secrets-scanning.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sourceanalyzer --version
+sourceanalyzer -b pre-commit -clean
+sourceanalyzer -b pre-commit *
+OUTPUT=$(sourceanalyzer -b pre-commit -scan -analyzers configuration)
+
+
+if [[ "$OUTPUT" == *"Password Management : Password"* ]] ; then
+  exit 1
+fi
 
 exit 0

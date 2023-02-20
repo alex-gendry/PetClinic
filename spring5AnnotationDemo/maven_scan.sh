3fea386fde55c32bd59f4b852c28cd8d9c1b00ba
Alex Gendry <alex.gendry@microfocus.com>
2023-02-20 12:54:12 +0000

    petclinic 3.0

--- spring5AnnotationDemo/maven_scan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mvn clean
-mvn com.fortify.sca.plugins.maven:sca-maven-plugin:20.1.4:clean
-mvn package com.fortify.sca.plugins.maven:sca-maven-plugin:20.1.4:translate
-mvn com.fortify.sca.plugins.maven:sca-maven-plugin:20.1.4:scan

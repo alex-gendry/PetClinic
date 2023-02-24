c2a211b5cec756a3c08353b7746a3fdb193abffd
Alex Gendry <alex.gendry@microfocus.com>
2023-02-24 13:15:56 +0000

    no stop

--- scan.sh
+++ scan.sh
@@ -5,7 +5,9 @@ set -x
 rm -f *.log *.taint *.err *.out
 
 sourceanalyzer -b test -clean
-sourceanalyzer -b test -verbose -debug -logfile trans.log -cp "petclinic-3.0/lib/*.jar" petclinic-3.0/src 
+
+mvn dependency:copy-dependencies -DoutputDirectory=lib
+sourceanalyzer -b test -verbose -debug -logfile trans.log -cp "lib/*.jar" src
 if [ "$1" == "debug" ]; then
     sourceanalyzer -b test -verbose -debug -logfile scan.log -scan -f petclinic.fpr -analyzers dataflow -fexec -Djava.awt.headless=false -DRuleScriptDebugger=true -DScriptsToDebug="wi_get_info" -Dcom.fortify.sca.rules.enable_wi_correlation # 2> scan.err 1> scan.out
 else
